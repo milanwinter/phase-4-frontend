2d42a4d384988500699f6931e91d15e0202324fc
AlexanderBowers <7319abowers@gmail.com>
2021-02-12 11:42:55 -0800

    post request works

---
+++ project-frontend/src/components/Playlist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Playlist = (videos) => {
+
+    return (
+        <div>
+            {null}
+        </div>
+    )
+
+}
+export default Playlist
--- project-frontend/src/containers/PlaylistContainer.js
+++ project-frontend/src/containers/PlaylistContainer.js
@@ -1,12 +1,57 @@
 import React ,{Component} from 'react'
 import {Route} from 'react-router-dom'
+import Playlist from '../components/Playlist'
 
 class PlaylistContainer extends Component {
+
+    componentDidMount() {
+        console.log('component did mount ran')
+        fetch(`http://localhost:3000/playlists`)
+        .then(res => res.json())
+        .then(playlists => this.setState({playlists}))
+    }
+
+    state = {
+        playlists: [],
+        newList: ''
+    }
+
+    handleChange = (e) => {
+        let newList = e.target.value
+        this.setState({newList})
+      }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:3000/playlists', {
+            method: "POST",
+            headers: {
+              "Content-Type" : "application/json",
+              "Accept" : "application/json"
+            },
+            body: JSON.stringify({
+              name: `${this.state.newList}`,
+              user_id: 1
+            })
+        }).then(res => res.json())
+        .then(console.log)
+}
 
 
     render() {
         return(
-            <div>This is Our Playlist Container</div>
+            <div>This is Our Playlist Container<br></br>
+                Form for new Playlist:
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Playlist name" onChange={this.handleChange}></input>
+                    <input type="submit" className="btn btn-primary"></input>
+                </form>
+                {this.state.playlists.length > 0 ? this.state.playlists.map(playlist => {
+                    <Playlist playlist={playlist}/>
+                }): null}
+
+            </div>
+
         )
     }
 }
