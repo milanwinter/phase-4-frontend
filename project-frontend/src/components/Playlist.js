885b394e108dcfa77d59ac4f1163d35158ca6263
AlexanderBowers <7319abowers@gmail.com>
2021-02-14 18:47:51 -0800

    show videos on playlist click

--- project-frontend/src/components/Playlist.js
+++ project-frontend/src/components/Playlist.js
@@ -3,13 +3,25 @@ import Video from './Video'
 
 class Playlist extends React.Component {
 
+    state = {
+        active: false
+    }
+    toggleActive= () => {
+        this.setState({
+            active: !this.state.active
+        })
+    }
+
    render () {
      return (
-        <div className="card" >
-            <p>{this.props.playlist.title}</p>
+        <div className="card"  >
+            <p onClick={() => {this.toggleActive()}}>{this.props.playlist.title}</p>
+            {this.state.active ? <div>
             { this.props.videos.map(video => {
              return  video.playlist_id == this.props.playlist.id ? <Video video={video}/> : null
             })}
+            </div>  : null}
+           
         </div>
     )
    }
