8cce228de16e33706c38947cceb94a5515edbb30
AlexanderBowers <7319abowers@gmail.com>
2021-02-16 11:55:03 -0800

    added button

--- project-frontend/src/components/Playlist.js
+++ project-frontend/src/components/Playlist.js
@@ -12,11 +12,31 @@ class Playlist extends React.Component {
         })
     }
 
+    handleLike = () => {
+        let token = localStorage.getItem("token")
+        let userId = localStorage.getItem("user")
+        fetch('http://localhost:3000/likes', {
+            method: "POST",
+            headers: {
+              "Authorization" : `Bearer ${token}`,
+              "Content-Type" : "application/json",
+              "Accept" : "application/json"
+            },
+            body: JSON.stringify({
+              playlist_id: this.props.playlist.id,
+              user_id: userId
+            })
+        }).then(res => res.json())
+        .then(console.log)
+    }
+
    render () {
      return (
         <div className="card"  >
-            <p onClick={() => {this.toggleActive()}}>{this.props.playlist.title}</p>
-            {this.state.active ? <div>
+            <p onClick={() => {this.toggleActive()}}>{this.props.playlist.title} Likes: {`${this.props.playlist.likes}`}</p>
+            {this.state.active ?
+             <div>
+                <button onClick={() => {this.handleLike()}} className= "btn-danger">Like</button>
             { this.props.videos.map(video => {
              return  video.playlist_id == this.props.playlist.id ? <Video video={video}/> : null
             })}
